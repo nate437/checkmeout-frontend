41d7344c506572a8e1b3762776c7211b7179b9d2
Laptop <spayne92@live.com>
2016-10-22 15:28:31 -0500

    Pushing changes. Still need api connection to data.

--- components/store-page.tsx
+++ components/store-page.tsx
@@ -6,26 +6,46 @@ import * as DOM from 'react-dom';
 
 import StoreThumbnail from './store-thumbnail.tsx';
 
-var Stores = React.createClass({
-  render: function() {
+interface StoreState{
+  data:string[];
+}
+
+class Stores extends React.Component<{}, StoreState>{
+  // getInitialState() {
+  //   return {data: []};
+  // },
+  // componentDidMount {
+  //   $.ajax({
+  //     url: 'api.checkmeout.dev/store',
+  //     dataType: 'json',
+  //     type: 'GET',
+  //     cache: false,
+  //     success(data) {
+  //       this.setState({data: data});
+  //     },
+  //     error(xhr, status, err) {
+  //       console.error('api.checkmeout.dev/store', status, err.toString())
+  //     }
+  //   })
+  // },
+  render() {
+    var data = [{"id":1,"name":"S&T Library","img_url":"https:\/\/media.glassdoor.com\/l\/a0\/c3\/2c\/36\/curtis-laws-wilson-library.jpg","location":"400 W 14th St, Rolla, MO 65409"}];
     return(
       <div>
         <div>
 
         </div>
+
         <div>
-          <StoreThumbnail />
-          <StoreThumbnail />
-          <StoreThumbnail />
-          <StoreThumbnail />
-          <StoreThumbnail />
+          <StoreThumbnail name={data[0].name} location={data[0].location} imgUrl={data[0].img_url} />
         </div>
+
         <div>
 
         </div>
       </div>
     )
   }
-});
+}
 
 export default Stores;
